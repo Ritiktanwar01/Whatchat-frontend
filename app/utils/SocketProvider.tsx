2921679b953b73cdb94a2a9660b8c1f42de54977
Ritik_tanwar_01 <tanwarritik999@gmail.com>
2025-10-16 16:52:28 +0530

    mini changes

--- app/utils/SocketProvider.tsx
+++ app/utils/SocketProvider.tsx
@@ -35,7 +35,7 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({ childr
         const socket = io(`${API_BASE_URL}`, {
             transports: ['websocket'],
             autoConnect: true,
-            query: {
+            auth: {
                 token // Replace with dynamic token logic
             },
         });
